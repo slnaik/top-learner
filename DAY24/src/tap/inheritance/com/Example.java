79352e8f8c46d02a4f5f07553786fcc3c59d9c47
acer <acer@DESKTOP-GDIQML6>
2023-05-24 17:28:18 +0530

    first commit

---
+++ DAY24/src/tap/inheritance/com/Example.java
@@ -0,0 +1,46 @@
+package tap.inheritance.com;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class Example
+{
+
+	public static void main(String[] args) 
+	{
+		
+
+	}
+
+}
